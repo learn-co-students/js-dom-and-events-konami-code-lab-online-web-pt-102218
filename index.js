b36c19acb9c5794621b60706bc6053212c46f260
github username <github email address>
2019-06-03 14:43:10 -0500

    Done.

--- index.js
+++ index.js
@@ -6,18 +6,20 @@ function init() {
   // Write your JavaScript code inside the init() function
 
   document.body.addEventListener('keydown', konamiCalculator)
-  
+
   function konamiCalculator(e){
     const key = parseInt(e.detail || e.which)
-    console.log('key', key)
-    if (key === code[index]) {
+    console.log(key)
+    if(key === code[index]){
       index++
-      if (index === code.length) {
-        alert("Hooray!!!")
-        index = 0
-      }
-    } else {
-      index = 0
+
+      if(index === code.length){
+        alert("KONAMI!");
+        index = 0;
+      };
     }
-  }
-};
+    else {
+      index = 0;
+    };
+  };
+}
